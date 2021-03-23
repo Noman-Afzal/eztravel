52a30f42e067884e6a0791161709ec010d91f520
akcybex <@Encrypted7465964@>
2021-03-23 17:39:02 +0500

    Footer fixed.

---
+++ resources/views/layouts/web.blade.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Adziv') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/web.css') }}" rel="stylesheet">
+    @stack('stylesheets')
+</head>
+<body>
+{{--<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">--}}
+{{--    <div class="container">--}}
+{{--        <a class="navbar-brand" href="{{ url('/') }}">--}}
+{{--            {{ config('app.name', 'Laravel') }}--}}
+{{--        </a>--}}
+{{--        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"--}}
+{{--                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">--}}
+{{--            <span class="navbar-toggler-icon"></span>--}}
+{{--        </button>--}}
+
+{{--        <div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
+{{--            <!-- Left Side Of Navbar -->--}}
+{{--            <ul class="navbar-nav mr-auto">--}}
+
+{{--            </ul>--}}
+
+{{--            <!-- Right Side Of Navbar -->--}}
+{{--            <ul class="navbar-nav ml-auto">--}}
+{{--                <!-- Authentication Links -->--}}
+{{--                @guest--}}
+{{--                    <li class="nav-item">--}}
+{{--                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>--}}
+{{--                    </li>--}}
+{{--                    @if (Route::has('register'))--}}
+{{--                        <li class="nav-item">--}}
+{{--                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>--}}
+{{--                        </li>--}}
+{{--                    @endif--}}
+{{--                @else--}}
+{{--                    <li class="nav-item dropdown">--}}
+{{--                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"--}}
+{{--                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>--}}
+{{--                            {{ Auth::user()->name }} <span class="caret"></span>--}}
+{{--                        </a>--}}
+
+{{--                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">--}}
+{{--                            <a class="dropdown-item" href="{{ route('logout') }}"--}}
+{{--                               onclick="event.preventDefault();--}}
+{{--                                                     document.getElementById('logout-form').submit();">--}}
+{{--                                {{ __('Logout') }}--}}
+{{--                            </a>--}}
+
+{{--                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">--}}
+{{--                                @csrf--}}
+{{--                            </form>--}}
+{{--                        </div>--}}
+{{--                    </li>--}}
+{{--                @endguest--}}
+{{--            </ul>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</nav>--}}
+@yield('navbar')
+@yield('content')
+<footer class="container-fluid py-4 bg-dark">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-12 col-lg-8 text-md-left text-lg-left align-self-center">
+                <ul class="list-inline list-unstyled mb-0">
+                    <li class="list-inline-item">
+                        <a href="#" class="text-white">Home</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="text-white">FAQ</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="text-white">Partner Program</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="text-white">PrivacyPolicy</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="text-white">Terms and Conditions</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-12 col-lg-4 text-md-right text-lg-right align-self-center">
+                <p class="text-white mb-0">© 2021 Adziv Digital, Limited. All Rights Reserved.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Scripts -->
+<script src="{{ asset('js/web.js') }}"></script>
+@stack('scripts')
+</body>
+</html>
